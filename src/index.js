1f1d842f02866ae053e0e608df415d3524ef39f7
AnhelinaDiakova <Gelja4ka@gmail.com>
2020-10-28 21:00:48 +0200

    Добавлякт refs.js

--- src/index.js
+++ src/index.js
@@ -1,39 +1,18 @@
-//При импорте шаблонов мы получаем функцию-шаблон готовую
-//принимать данные и возвращать строку разметки
 import menuCardsTpl from './templates/menu-card.hbs';
 import menuMarkup from './menu.json';
+import { refs } from './refs'
 import './styles.css';
-// console.log(menuCardsTpl)
-
-const refs = {
-  menuList: document.querySelector('.js-menu'),
-  input: document.querySelector('#theme-switch-toggle'),
-  body: document.querySelector('body'),
-}
-
 
 const Theme = {
   LIGHT: 'light-theme',
   DARK: 'dark-theme',
 };
 
-
-// const menuCardsMarkup = createMenuCard(menuMarkup);
 const createMenuCardsMarkup = menuCardsTpl(menuMarkup)
 
 refs.menuList.insertAdjacentHTML('beforeend', createMenuCardsMarkup);
 refs.input.addEventListener('change', onClickCheckbox)
-addLocalStorageTheme(); 
-
-// function createMenuCard(menuMarkup) {
-// // return menuMarkup.map(menu => menuCard(menu)).join('');
-// //menuCardsTpl под капотом колбек функция, туда приходит menu
-//   // return menuMarkup.map(menuCardsTpl).join('');
-
-//   return menuCardsTpl(menuMarkup)
-// }
-
-
+addLocalStorageTheme();
 
 function onClickCheckbox(e) {
   console.log(e.currentTarget.checked)
